7d389e40acdd4d8e9ff5807c335d36e44e8c22cc
Mickael Pham <mickael.pham@outlook.com>
2014-07-07 23:07:38 -0700

    Started dialpad problem

---
+++ src/main/java/problems/PhoneDialPad.java
@@ -0,0 +1,14 @@
+package problems;
+
+/**
+ * You are given a function that can tell you if a String is a valid English
+ * word or not. Given a number, corresponding to a dialpad input, write a method
+ * that can list all the word possibles with it.
+ * 
+ * E.g. the entry "2225" can be translated as "BACK" on the dialpad.
+ * 
+ * Hint: use recursion.
+ */
+public class PhoneDialPad {
+
+}
