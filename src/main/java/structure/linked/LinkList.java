7bfbc93dcd1b71a11d4e4f8d9a733e2ffc133513
Mickael Pham <mickael.pham@outlook.com>
2014-07-16 14:36:44 -0700

    Added test for find and deletion with key in LinkList

--- src/main/java/structure/linked/LinkList.java
+++ src/main/java/structure/linked/LinkList.java
@@ -25,6 +25,41 @@ public class LinkList {
 		return tmp;
 	}
 
+	public Link find(int key) {
+		if (isEmpty())
+			return null;
+		Link current = first;
+		while (current != null) {
+			if (current.getIData() == key)
+				return current;
+			else
+				current = current.next;
+		}
+		// Reach the end of the list, could not find the key.
+		return null;
+	}
+	
+	public Link delete(int key) {
+		if (isEmpty())
+			return null;
+		else if (first.getIData() == key)
+			return deleteFirst();
+		else {
+			Link previous = first;
+			Link current = first.next;
+			while (current != null) {
+				if (current.getIData() == key) {
+					previous.next = current.next;
+					return current;
+				} else {
+					previous = current;
+					current = current.next;
+				}
+			}
+			return null;
+		}
+	}
+
 	public void displayList() {
 		System.out.print("List (first-->last): ");
 		if (!isEmpty())
--- src/test/java/structure/linked/LinkListTest.java
+++ src/test/java/structure/linked/LinkListTest.java
@@ -14,20 +14,39 @@ public class LinkListTest {
 		theList.insertFirst(66, 6.99);
 		theList.insertFirst(88, 8.99);
 		
-		theList.displayList();
+//		theList.displayList();
 		
 		double[] deleted = new double[4];
 		int indexDeleted = 0;
 		while (!theList.isEmpty()) {
 			Link del = theList.deleteFirst();
 			deleted[indexDeleted++] = del.getDData();
-			System.out.print("Deleted: ");
-			del.displayLink();
-			System.out.println("");
+//			System.out.print("Deleted: ");
+//			del.displayLink();
+//			System.out.println("");
 		}
 		
 		double[] expected = new double[] { 8.99, 6.99, 4.99, 2.99 };
 		Assert.assertArrayEquals(expected, deleted, 0);
+	}
+	
+	@Test
+	public void testFindAndDeletionWithKey() {
+		LinkList theList = new LinkList();
+		
+		theList.insertFirst(22, 2.99);
+		theList.insertFirst(44, 4.99);
+		theList.insertFirst(66, 6.99);
+		theList.insertFirst(88, 8.99);
+		
+//		theList.displayList();
+		Link deleted = theList.delete(66);
+//		theList.displayList();
+		
+		Assert.assertEquals(6.99, deleted.getDData(), 0);
+		
+		deleted = theList.delete(22);
+		Assert.assertEquals(2.99, deleted.getDData(), 0);
 	}
 
 }
