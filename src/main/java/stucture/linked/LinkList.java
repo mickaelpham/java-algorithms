8d66aeb230de5b12c707f3499430fb9d15d69e73
Mickael Pham <mickael.pham@outlook.com>
2014-07-16 13:52:05 -0700

    Added insertFirst() and deleteFirst() methods to LinkList

--- src/main/java/stucture/linked/LinkList.java
+++ src/main/java/stucture/linked/LinkList.java
@@ -2,4 +2,40 @@ package stucture.linked;
 
 public class LinkList {
 
+	// Reference to first link on the list.
+	private Link first;
+
+	public LinkList() {
+		first = null;
+	}
+
+	public boolean isEmpty() {
+		return (first == null);
+	}
+
+	public void insertFirst(int id, double dd) {
+		Link link = new Link(id, dd);
+		link.next = first;
+		first = link;
+	}
+
+	public Link deleteFirst() {
+		Link tmp = first;
+		first = first.next;
+		return tmp;
+	}
+
+	public void displayList() {
+		System.out.print("List (first-->last): ");
+		if (!isEmpty())
+			displayRecursive(first);
+		System.out.println("");
+	}
+
+	private void displayRecursive(Link link) {
+		link.displayLink();
+		if (link.next != null)
+			displayRecursive(link.next);
+	}
+
 }
