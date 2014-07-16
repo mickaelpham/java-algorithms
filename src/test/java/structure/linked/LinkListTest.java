00094bb15b3face2bb2ae9e60c7affd71bcb0330
Mickael Pham <mickael.pham@outlook.com>
2014-07-16 13:58:47 -0700

    Added unit test for simple linked list

---
+++ src/test/java/structure/linked/LinkListTest.java
@@ -0,0 +1,33 @@
+package structure.linked;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinkListTest {
+	
+	@Test
+	public void simpleLinkListTest() {
+		LinkList theList = new LinkList();
+		
+		theList.insertFirst(22, 2.99);
+		theList.insertFirst(44, 4.99);
+		theList.insertFirst(66, 6.99);
+		theList.insertFirst(88, 8.99);
+		
+		theList.displayList();
+		
+		double[] deleted = new double[4];
+		int indexDeleted = 0;
+		while (!theList.isEmpty()) {
+			Link del = theList.deleteFirst();
+			deleted[indexDeleted++] = del.getDData();
+			System.out.print("Deleted: ");
+			del.displayLink();
+			System.out.println("");
+		}
+		
+		double[] expected = new double[] { 8.99, 6.99, 4.99, 2.99 };
+		Assert.assertArrayEquals(expected, deleted, 0);
+	}
+
+}
