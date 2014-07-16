d05eed7770c33da1feabb9d8c6d425f07aa55f16
Mickael Pham <mickael.pham@outlook.com>
2014-07-16 14:56:49 -0700

    Added test for insertion in last position in the link list

--- src/test/java/structure/linked/LinkListTest.java
+++ src/test/java/structure/linked/LinkListTest.java
@@ -4,49 +4,70 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LinkListTest {
-	
+
 	@Test
 	public void simpleLinkListTest() {
 		LinkList theList = new LinkList();
-		
+
 		theList.insertFirst(22, 2.99);
 		theList.insertFirst(44, 4.99);
 		theList.insertFirst(66, 6.99);
 		theList.insertFirst(88, 8.99);
-		
-//		theList.displayList();
-		
+
+		// theList.displayList();
+
 		double[] deleted = new double[4];
 		int indexDeleted = 0;
 		while (!theList.isEmpty()) {
 			Link del = theList.deleteFirst();
 			deleted[indexDeleted++] = del.getDData();
-//			System.out.print("Deleted: ");
-//			del.displayLink();
-//			System.out.println("");
+			// System.out.print("Deleted: ");
+			// del.displayLink();
+			// System.out.println("");
 		}
-		
+
 		double[] expected = new double[] { 8.99, 6.99, 4.99, 2.99 };
 		Assert.assertArrayEquals(expected, deleted, 0);
 	}
-	
+
 	@Test
 	public void testFindAndDeletionWithKey() {
 		LinkList theList = new LinkList();
-		
+
 		theList.insertFirst(22, 2.99);
 		theList.insertFirst(44, 4.99);
 		theList.insertFirst(66, 6.99);
 		theList.insertFirst(88, 8.99);
-		
-//		theList.displayList();
+
+		// theList.displayList();
 		Link deleted = theList.delete(66);
-//		theList.displayList();
-		
+		// theList.displayList();
+
 		Assert.assertEquals(6.99, deleted.getDData(), 0);
-		
+
 		deleted = theList.delete(22);
 		Assert.assertEquals(2.99, deleted.getDData(), 0);
+	}
+
+	@Test
+	public void testInsertLast() {
+		LinkList theList = new LinkList();
+
+		theList.insertLast(22, 2.99);
+		theList.insertLast(44, 4.99);
+		theList.insertLast(66, 6.99);
+		theList.insertLast(88, 8.99);
+
+
+		double[] deleted = new double[4];
+		int indexDeleted = 0;
+		while (!theList.isEmpty()) {
+			Link del = theList.deleteFirst();
+			deleted[indexDeleted++] = del.getDData();
+		}
+
+		double[] expected = new double[] { 2.99, 4.99, 6.99, 8.99 };
+		Assert.assertArrayEquals(expected, deleted, 0);
 	}
 
 }
